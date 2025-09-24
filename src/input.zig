d3582a1e5fe3ed14ba4f2bee92c615827afdb894
James Kienle <james.kienle@gmail.com>
2025-09-24 16:15:29 -0700

    Finish Input Bindings

--- src/input.zig
+++ src/input.zig
@@ -4,96 +4,71 @@ const RgfwKey = rgfw.RgfwKey;
 const RgfwMouseButton = rgfw.RgfwMouseButton;
 const RgfwWindow = rgfw.RgfwWindow;
 
-/// @brief No brief description was provided by RGFW.
+// MARK: Functions
+
+/// Get the current scroll value (of the frame).
 ///
-/// @remarks The original name for this function in RGFW is `RGFW_isClicked`.
+/// @remarks The original name for this function in RGFW is `RGFW_getMouseScroll`.
+pub fn rgfwGetMouseScroll(x: *f32, y: *f32) bool {
+    return RGFW_getMouseScroll(x, y);
+}
+
+/// Get the current vector value (of the frame).
 ///
-/// @see [The RGFW Docs](https://colleagueriley.github.io/RGFW/docs/group__Input.html#ga04986425d9881d78ba3d4f6e2c8a4368)
-pub fn rgfwIsClicked(win: *RgfwWindow, key: RgfwKey) bool {
-    return RGFW_isClicked(win, key);
+/// @remarks The original name for this function in RGFW is `RGFW_getMouseVector`.
+pub fn rgfwGetMouseVector(x: *f32, y: *f32) bool {
+    return RGFW_getMouseVector(x, y);
 }
 
 /// @brief If key is held (key code).
 ///
-/// @remarks The original name for this function in RGFW is `RGFW_isHeld`.
-///
-/// @see [The RGFW Docs](https://colleagueriley.github.io/RGFW/docs/group__Input.html#ga6fe119d8239e7cc6fdf07ae378e2658b)
-pub fn rgfwIsHeld(win: *RgfwWindow, key: RgfwKey) bool {
-    return RGFW_isHeld(win, key);
+/// @remarks The original name for this function in RGFW is `RGFW_isKeyDown`.
+pub fn rgfwIsKeyDown(key: RgfwKey) bool {
+    return RGFW_isKeyDown(key);
 }
 
-/// @brief If a mouse button is held.
+/// @brief If key is pressed during the current frame (key code).
 ///
-/// @remarks The original name for this function in RGFW is `RGFW_isMouseHeld`.
-///
-/// @see [The RGFW Docs](https://colleagueriley.github.io/RGFW/docs/group__Input.html#gab88bec0f869a9000ed3703a7bfe61278)
-pub fn rgfwIsMouseHeld(win: *RgfwWindow, button: RgfwMouseButton) bool {
-    return RGFW_isMouseHeld(win, button);
+/// @remarks The original name for this function in RGFW is `RGFW_isKeyPressed`.
+pub fn rgfwIsKeyPressed(key: RgfwKey) bool {
+    return RGFW_isKeyPressed(key);
 }
 
-/// @brief If a mouse button is pressed.
+/// @brief If key is released (key code)
+///
+/// @remarks The original name for this function in RGFW is `RGFW_isKeyReleased`.
+pub fn rgfwIsKeyReleased(key: RgfwKey) bool {
+    return RGFW_isKeyReleased(key);
+}
+
+/// @brief If a mouse button is down.
+///
+/// @remarks The original name for this function in RGFW is `RGFW_isMouseDown`.
+pub fn rgfwIsMouseDown(button: RgfwMouseButton) bool {
+    return RGFW_isMouseDown(button);
+}
+
+/// @brief If a mouse button is pressed during the current frame.
 ///
 /// @remarks The original name for this function in RGFW is `RGFW_isMousePressed`.
-///
-/// @see [The RGFW Docs](https://colleagueriley.github.io/RGFW/docs/group__Input.html#ga730c6f8a9a8815c2f02288ccf51d41ad)
-pub fn rgfwIsMousePressed(win: *RgfwWindow, button: RgfwMouseButton) bool {
-    return RGFW_isMousePressed(win, button);
+pub fn rgfwIsMousePressed(button: RgfwMouseButton) bool {
+    return RGFW_isMousePressed(button);
 }
 
 /// @brief If a mouse button was released
 ///
 /// @remarks The original name for this function in RGFW is `RGFW_isMouseReleased`.
-///
-/// @see [The RGFW Docs](https://colleagueriley.github.io/RGFW/docs/group__Input.html#gac3ad11fe46449b780172cea27350b324)
 pub fn rgfwIsMouseReleased(win: *RgfwWindow, button: RgfwMouseButton) bool {
     return RGFW_isMouseReleased(win, button);
 }
 
-/// @brief If key is pressed (key code).
-///
-/// @details If window == `null`, it checks if the key is pressed globally. Otherwise, it checks
-///          only if the key is pressed while the window in focus.
-///
-/// @remarks The original name for this function in RGFW is `RGFW_isPressed`.
-///
-/// @see [The RGFW Docs](https://colleagueriley.github.io/RGFW/docs/group__Input.html#ga6edb31c11775f450c7f3554771fbc252)
-pub fn rgfwIsPressed(win: *RgfwWindow, key: RgfwKey) bool {
-    return RGFW_isPressed(win, key);
-}
+// MARK: C Functions
 
-/// @brief If key is released (key code)
-///
-/// @remarks The original name for this function in RGFW is `RGFW_isReleased`.
-///
-/// @see [The RGFW Docs](https://colleagueriley.github.io/RGFW/docs/group__Input.html#ga379b35a3e3d4d5b230aa2b22fc2d5e0c)
-pub fn rgfwIsReleased(win: *RgfwWindow, key: RgfwKey) bool {
-    return RGFW_isReleased(win, key);
-}
-
-/// @brief If a mouse button was pressed (checks previous state only).
-///
-/// @remarks The original name for this function in RGFW is `RGFW_wasMousePressed`.
-///
-/// @see [The RGFW Docs](https://colleagueriley.github.io/RGFW/docs/group__Input.html#ga9f2cce6197de5cb5a46d67e5caeb3701)
-pub fn rgfwWasMousePressed(win: *RgfwWindow, button: RgfwMouseButton) bool {
-    return RGFW_wasMousePressed(win, button);
-}
-
-/// @brief If key was pressed (checks previous state only) (key code).
-///
-/// @remarks The original name for this function in RGFW is `RGFW_wasPressed`.
-///
-/// @see [The RGFW Docs](https://colleagueriley.github.io/RGFW/docs/group__Input.html#ga9940f3a1b5e9918c1639a2986a120f33)
-pub fn rgfwWasPressed(win: *RgfwWindow, key: RgfwKey) bool {
-    return RGFW_wasPressed(win, key);
-}
-
-extern "rgfw" fn RGFW_isClicked(win: *RgfwWindow, key: RgfwKey) callconv(.c) bool;
-extern "rgfw" fn RGFW_isHeld(win: *RgfwWindow, key: RgfwKey) callconv(.c) bool;
-extern "rgfw" fn RGFW_isMouseHeld(win: *RgfwWindow, button: RgfwMouseButton) callconv(.c) bool;
-extern "rgfw" fn RGFW_isMousePressed(win: *RgfwWindow, button: RgfwMouseButton) callconv(.c) bool;
-extern "rgfw" fn RGFW_isMouseReleased(win: *RgfwWindow, button: RgfwMouseButton) callconv(.c) bool;
-extern "rgfw" fn RGFW_isPressed(win: *RgfwWindow, key: RgfwKey) callconv(.c) bool;
-extern "rgfw" fn RGFW_isReleased(win: *RgfwWindow, key: RgfwKey) callconv(.c) bool;
-extern "rgfw" fn RGFW_wasMousePressed(win: *RgfwWindow, button: RgfwMouseButton) callconv(.c) bool;
-extern "rgfw" fn RGFW_wasPressed(win: *RgfwWindow, key: RgfwKey) callconv(.c) bool;
+extern "rgfw" fn RGFW_getMouseScroll(x: *f32, y: *f32) callconv(.c) bool;
+extern "rgfw" fn RGFW_getMouseVector(x: *f32, y: *f32) callconv(.c) bool;
+extern "rgfw" fn RGFW_isMouseDown(button: RgfwMouseButton) callconv(.c) bool;
+extern "rgfw" fn RGFW_isMousePressed(button: RgfwMouseButton) callconv(.c) bool;
+extern "rgfw" fn RGFW_isMouseReleased(button: RgfwMouseButton) callconv(.c) bool;
+extern "rgfw" fn RGFW_isKeyDown(key: RgfwKey) callconv(.c) bool;
+extern "rgfw" fn RGFW_isKeyPressed(key: RgfwKey) callconv(.c) bool;
+extern "rgfw" fn RGFW_isKeyReleased(key: RgfwKey) callconv(.c) bool;
